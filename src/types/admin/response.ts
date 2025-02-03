3cbd9a0fe7ff0de80fffcb94d80079e4f561ae44
coder-unani <coder.unani@gmail.com>
2025-02-04 00:55:08 +0900

    [fix] 프로젝트 구조 변경으로 인한 삭제

--- src/types/admin/response.ts
+++
@@ -1,9 +0,0 @@
-type typeErrorCode = 'ERR' | 'FAIL_SERVER' | 'FAIL_VALIDATION';
-type typeMessage = string | undefined;
-
-export interface IServiceResponse<T = undefined> {
-  result: boolean;
-  code?: typeErrorCode;
-  message?: typeMessage;
-  data?: T;
-}
