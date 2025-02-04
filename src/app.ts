4b91651de4dafa877f94342f2aa5447f177e79e1
coder-unani <coder.unani@gmail.com>
2025-02-05 02:59:33 +0900

    [refactor] 환경 설정 값 변수화

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,14 @@ import apiFrontendRouter from './routes/api/frontend';
 import apiBackendRouter from './routes/api/backend';
 import frontendRouter from './routes/web/frontend';
 import backendRouter from './routes/web/backend';
+import { SERVICE_PORT, STATIC_PATH, LOG_PATH, LOG_LEVEL, CORS_OPTIONS, CORS_BACKEND_OPTIONS } from './config/config';
 
 /**
  * 필요한 환경 변수 설정
  */
-const EXPRESS_PORT: number = process.env.EXPRESS_PORT ? parseInt(process.env.EXPRESS_PORT) : 3000;
+if (!SERVICE_PORT || !STATIC_PATH || !LOG_PATH || !LOG_LEVEL) {
+  throw new Error('필수 환경변수가 설정되지 않았습니다.');
+}
 
 /**
  * Express 앱 설정
@@ -25,7 +28,7 @@ const app: Application = express();
  * 공통 미들웨어 설정
  */
 // Http Logger 설정
-const logger = new ExpressLogger();
+const logger = new ExpressLogger(LOG_PATH, LOG_LEVEL);
 const loggerMiddleware: IMiddleware = new LoggerMiddleware(logger);
 app.use((req, res, next) => loggerMiddleware.handle(req, res, next));
 // Body-parser 설정
@@ -36,19 +39,16 @@ app.use(express.json()); // JSON 데이터 파싱
  * API 설정
  */
 // API CORS 설정
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-}
-const corsMiddleware: IMiddleware = new CorsMiddleware(corsOptions);
+const corsMiddleware: IMiddleware = new CorsMiddleware(CORS_OPTIONS);
+const backendCorsMiddleware: IMiddleware = new CorsMiddleware(CORS_BACKEND_OPTIONS);
 apiFrontendRouter.use((req, res, next) => corsMiddleware.handle(req, res, next));
-apiBackendRouter.use((req, res, next) => corsMiddleware.handle(req, res, next));
+apiBackendRouter.use((req, res, next) => backendCorsMiddleware.handle(req, res, next));
 
 /**
  * WEB 설정
  */
 // 정적 파일 설정
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, STATIC_PATH))); 
 // 템플릿 엔진 (WEB 서비스가 없을 경우 삭제)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -75,6 +75,6 @@ backendRouter.use((err: any, req: Request, res: Response, next: NextFunction) =>
 /**
  * 서버 실행
  */
-app.listen(EXPRESS_PORT, () => {
-  console.log(`Server is running at http://localhost:${EXPRESS_PORT}`);
+app.listen(SERVICE_PORT, () => {
+  console.log(`Server is running at http://localhost:${SERVICE_PORT}`);
 });
