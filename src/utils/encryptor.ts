85f4d8201ce2dbf1c43008761955dfc89cdf8df7
coder-unani <coder.unani@gmail.com>
2025-02-06 00:15:14 +0900

    [feat] 암화화 관련 함수들 개발

---
+++ src/utils/encryptor.ts
@@ -0,0 +1,57 @@
+import bcrypt from 'bcryptjs';
+import CryptoJS from 'crypto-js';
+
+import { CRYPTO_SECRET_KEY } from '../config/config';
+
+/**
+ * 비밀번호를 해싱하는 함수
+ * @param password - 평문 비밀번호
+ * @returns Promise<string> - 해싱된 비밀번호
+ */
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRound = 10;
+  const salt = await bcrypt.genSalt(saltRound);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+
+/**
+ * 비밀번호를 검증하는 함수
+ * @param plainPassword - 사용자가 입력한 평문 비밀번호
+ * @param hashedPassword - 데이터베이스에 저장된 해싱된 비밀번호
+ * @returns Promise<boolean> - 비밀번호가 일치하면 true, 그렇지 않으면 false
+ */
+export const verifyPassword = async (
+  plainPassword: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
+  return isMatch;
+};
+
+/**
+ * 평문 데이터 암호화 함수
+ * @param plainData - 평문 데이터
+ * @returns string - 암호화된 데이터
+ */
+export const encryptDataAES = (plainData: string): string => {
+  return CryptoJS.AES.encrypt(plainData, CRYPTO_SECRET_KEY).toString();
+};
+
+/**
+ * 평문 데이터 복호화 함수
+ * @param encryptedData - 암호화된 데이터
+ * @returns string - 복호화된 데이터
+ */
+export const decryptDataAES = (encryptedData: string): string => {
+  const bytes = CryptoJS.AES.decrypt(encryptedData, CRYPTO_SECRET_KEY);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
+
+// 테스트 코드
+// const phoneNumber = '010-1234-5678';
+// const encryptedPhoneNumber = encryptDataAES(phoneNumber);
+// console.log('Encrypted:', encryptedPhoneNumber);
+
+// const decryptedPhoneNumber = decryptDataAES(encryptedPhoneNumber);
+// console.log('Decrypted:', decryptedPhoneNumber);
