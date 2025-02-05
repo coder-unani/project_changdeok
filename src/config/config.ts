b95f9b6f70b92d0881a91294ed5516f79e7cf75d
coder-unani <coder.unani@gmail.com>
2025-02-06 00:13:26 +0900

    [feat] 환경변수 추가

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,3 +10,14 @@ export const CORS_BACKEND_OPTIONS = {
   origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
   optionsSuccessStatus: 200,
 }
+
+// Crypto Secret Key
+export const CRYPTO_SECRET_KEY = (process.env.CRYPTO_SECRET) ? process.env.CRYPTO_SECRET : ''; 
+
+// 허용할 HTML 태그
+export const ALLOWED_TAGS = [
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul',
+  'ol', 'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code',
+  'hr', 'br', 'div', 'table', 'thead', 'caption', 'tbody', 'tr',
+  'th', 'td', 'pre', 'iframe', 'img', 'span', 'font', 'del', 'ins'
+];
