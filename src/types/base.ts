d840d6f96a66cf3e1cb005dc0ce7b1656172c14d
coder-unani <coder.unani@gmail.com>
2025-02-07 05:44:16 +0900

    [feat] 사이트 기본 설정 정보 추가

---
+++ src/types/base.ts
@@ -0,0 +1,9 @@
+export interface ICompanyInfo {
+  name: string; // 회사명
+  businessNumber: string; // 사업자 번호
+  ceo: string; // 대표자
+  address: string; // 주소
+  tel: string; // 전화번호
+  email: string; // 이메일
+  logo: string; // 로고
+}
