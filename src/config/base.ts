efa9d59ef98d952f2c63c0046c6240dc4dbb1ce4
coder-unani <coder.unani@gmail.com>
2025-02-07 05:41:52 +0900

    [feat] 사이트 기본 설정 정보 추가

---
+++ src/config/base.ts
@@ -0,0 +1,11 @@
+import { ICompanyInfo } from '../types/base';
+
+export const companyInfo: ICompanyInfo = {
+  name: '오르빗코드',
+  businessNumber: '000-00-00000',
+  ceo: '대표자',
+  address: '주소',
+  tel: '000-000-0000',
+  email: 'groot@orbitcode.kr',
+  logo: '',
+};
