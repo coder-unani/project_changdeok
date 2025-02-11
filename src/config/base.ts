ecda660a0ba755a959e6065e6b7738032e496237
coder-unani <coder.unani@gmail.com>
2025-02-11 21:56:18 +0900

    [refactor] 기본 정보 항목 추가

--- src/config/base.ts
+++ src/config/base.ts
@@ -2,10 +2,11 @@ import { ICompanyInfo } from '../types/base';
 
 export const companyInfo: ICompanyInfo = {
   name: '오르빗코드',
+  nameEn: 'Orbitcode',
   businessNumber: '000-00-00000',
   ceo: '대표자',
   address: '주소',
   tel: '000-000-0000',
   email: 'groot@orbitcode.kr',
-  logo: '',
+  logo: '/img/logo.svg',
 };
