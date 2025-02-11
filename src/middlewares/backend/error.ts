964045cac888a042b35be5f3315fe66a2f9df471
coder-unani <coder.unani@gmail.com>
2025-02-11 21:58:17 +0900

    [feat] 백엔드 에러 페이지 개발

--- src/middlewares/backend/error.ts
+++ src/middlewares/backend/error.ts
@@ -1,14 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
+
 import { IErrorMiddleware } from '../../types/middleware';
 import { IHttpLogger } from '../../types/logger';
+import { backendRoutes } from '../../routes/routes';
 
+// TODO: 에러 처리 미들웨어가 동작하지 않는 이유 찾아볼 것
+/**
+ * 분석 결과. Express에서 에러 처리 미들웨어는 라우터에 등록된 순서대로 동작한다.
+ * 따라서 에러 처리 미들웨어는 라우터 등록 후에 등록해야 한다.
+ * 이미 그렇게 했지만 동작하지 않는다.
+ * Express 에서는 비동기 함수의 에러를 캐치하지 못하는 경우가 있다.
+ * 이에 대한 처리 방법 연구가 필요하다.
+ * 일단 임시로 각 라우터에 error 함수를 만들어서 try-catch에서 catch 문에서 error 함수를 호출하는 형태로 만든다.
+ */
 export class ErrorMiddleware implements IErrorMiddleware {
   private logger: IHttpLogger;
-  private template: string;
 
-  constructor(logger: IHttpLogger, template: string = 'backend/error') {
+  constructor(logger: IHttpLogger) {
+    console.log("ErrorMiddleware.constructor");
     this.logger = logger;
-    this.template = template;
 
   }
 
@@ -16,14 +26,19 @@ export class ErrorMiddleware implements IErrorMiddleware {
     // 에러 로그 기록
     this.logger.logException(req, err);
 
+    console.log("err = ", err);
+
     // 응답
+    const { view, layout } = backendRoutes.error;
     res.status(500).render(
-      this.template, 
+      view, 
       { 
+        layout,
         status: err.status || 500, 
         message: err.message || '일시적인 오류가 발생했습니다.' 
       }
     );
 
+    // 다음 미들웨어로 이동하지 않고 종료
   }
 }
