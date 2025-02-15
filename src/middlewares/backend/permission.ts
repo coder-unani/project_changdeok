8fa363e613475ecb747964b9a29634b0f47ebc13
coder-unani <coder.unani@gmail.com>
2025-02-16 04:08:06 +0900

    [feat] 메뉴 접근 권한 구현

---
+++ src/middlewares/backend/permission.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { backendRoutes } from '../../routes/routes';
+import { IMiddleware } from '../../types/middleware';
+
+export class PermissionMiddleware implements IMiddleware {
+  constructor() { }
+
+  public handle(req: Request, res: Response, next: NextFunction): void {
+    // 권한 확인
+    console.log("path = ", req.path);
+
+    // 다음 미들웨어로 이동
+    next();
+  }
+}
