d6db5b77abb87064b1b51ed19752d468758018f0
coder-unani <coder.unani@gmail.com>
2025-04-16 17:36:30 +0900

    [fix] img src, width, height 속성 허용

--- src/middlewares/sanitizer.ts
+++ src/middlewares/sanitizer.ts
@@ -5,20 +5,19 @@ import { IMiddleware } from '../types/middleware';
 
 // SanitizeMiddleware 클래스
 export class SanitizeMiddleware implements IMiddleware {
-
   private allowedTags: string[];
 
-  constructor(allowedTags: string[] = []) { 
+  constructor(allowedTags: string[] = []) {
     this.allowedTags = allowedTags;
   }
-  
+
   public handle(req: Request, res: Response, next: NextFunction): void {
     if (req.body) {
       for (const key in req.body) {
         if (typeof req.body[key] === 'string') {
           req.body[key] = sanitizeHtml(req.body[key], {
             allowedTags: this.allowedTags, // 허용할 HTML 태그
-            allowedAttributes: {}, // 모든 속성 제거
+            allowedAttributes: { a: ['href', 'target'], img: ['src', 'width', 'height'] }, // 속성 허용
             disallowedTagsMode: 'discard', // 허용되지 않은 태그 삭제
           });
         }
