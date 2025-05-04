07c4783c97a825b55870e8c61455a6bbcafeb01c
coder-unani <coder.unani@gmail.com>
2025-05-04 20:26:24 +0900

    [refactor] 코드 사용성 개선

--- src/services/contentService.ts
+++ src/services/contentService.ts
@@ -30,29 +30,22 @@ export class ContentService extends BaseService implements IContentService {
 
       // 컨텐츠 그룹 정보 조회
       const groupInfo = await this.groupInfo(groupId);
-
       if (!groupInfo.result) {
         throw new AppError(groupInfo.code, groupInfo.message);
-      }
-
-      // 컨텐츠 암호화 여부 확인
-      if (groupInfo.data?.isEncrypt) {
-        // 컨텐츠 암호화
-        data.content = encryptDataAES(data.content);
       }
 
       // 컨텐츠 생성
       await this.prisma.content.create({
         data: {
-          groupId: groupId,
+          groupId,
           title: data.title.trim(),
-          content: data.content?.trim(),
+          content: groupInfo.data?.isEncrypt ? encryptDataAES(data.content) : data.content?.trim(),
           ip: data.ip || '',
           userAgent: data.userAgent || '',
         },
       });
 
-      // 성공
+      // 응답 성공
       return { result: true };
     } catch (error) {
       return this.handleError(error);
@@ -66,17 +59,13 @@ export class ContentService extends BaseService implements IContentService {
         where: {
           id: contentId,
           isDeleted: false,
+          isActivated: true,
         },
       });
 
       // 컨텐츠가 없는 경우
       if (!prismaContent) {
         throw new NotFoundError('컨텐츠가 존재하지 않습니다.');
-      }
-
-      // 컨텐츠 활성화 여부 확인
-      if (!prismaContent.isActivated) {
-        throw new NotFoundError('비활성화된 컨텐츠입니다.');
       }
 
       // 컨텐츠 그룹 정보 조회
@@ -93,11 +82,7 @@ export class ContentService extends BaseService implements IContentService {
         throw new NotFoundError('컨텐츠 그룹이 존재하지 않습니다.');
       }
 
-      // 날짜 형식 변환
-      const createdAtToString = formatDateToString(prismaContent.createdAt.toISOString(), true, true, true);
-      const updatedAtToString = formatDateToString(prismaContent.updatedAt?.toISOString(), true, true, true);
-
-      // 컨텐츠 정보 생성
+      // 컨텐츠 생성
       const content: IContent = {
         id: prismaContent.id,
         groupId: prismaContent.groupId,
@@ -115,11 +100,13 @@ export class ContentService extends BaseService implements IContentService {
         isActivated: prismaContent.isActivated,
         ip: prismaContent.ip || null,
         userAgent: prismaContent.userAgent || null,
-        createdAt: createdAtToString as string,
-        updatedAt: (updatedAtToString as string) || null,
+        createdAt: formatDateToString(prismaContent.createdAt.toISOString(), true, true, true) as string,
+        updatedAt: prismaContent.updatedAt
+          ? (formatDateToString(prismaContent.updatedAt.toISOString(), true, true, true) as string)
+          : null,
       };
 
-      // 성공
+      // 응답 성공
       return {
         result: true,
         metadata: {
@@ -145,39 +132,37 @@ export class ContentService extends BaseService implements IContentService {
       // 업데이트 데이터 생성
       const updateData: Partial<IRequestContentUpdate> = {};
 
-      if (data.title !== undefined) updateData.title = data.title;
-      if (data.content !== undefined) updateData.content = data.content;
-      if (data.isAnonymous !== undefined) updateData.isAnonymous = data.isAnonymous;
-      if (data.isActivated !== undefined) updateData.isActivated = data.isActivated;
-
-      // 제목 길이 검증
-      if (updateData.title) {
-        const validateTitle = validateStringLength(updateData.title, 1, 50);
+      // 업데이트할 필드만 추가
+      if (data.title !== undefined) {
+        const validateTitle = validateStringLength(data.title, 1, 50);
         if (!validateTitle.result) {
           throw new ValidationError(validateTitle.message);
         }
+        updateData.title = data.title;
       }
 
       // 내용 길이 검증
-      if (updateData.content) {
-        const validateContent = validateStringLength(updateData.content, 1, 1000);
+      if (data.content !== undefined) {
+        const validateContent = validateStringLength(data.content, 1, 1000);
         if (!validateContent.result) {
           throw new ValidationError(validateContent.message);
         }
+        updateData.content = data.content;
       }
+
+      // 익명 여부 검증
+      if (data.isAnonymous !== undefined) updateData.isAnonymous = data.isAnonymous;
+
+      // 활성화 여부 검증
+      if (data.isActivated !== undefined) updateData.isActivated = data.isActivated;
 
       // 컨텐츠 업데이트
       await this.prisma.content.update({
-        where: {
-          id: contentId,
-        },
-        data: {
-          ...updateData,
-          updatedAt: new Date(),
-        },
+        where: { id: contentId },
+        data: { ...updateData, updatedAt: new Date() },
       });
 
-      // 성공
+      // 응답 성공
       return { result: true };
     } catch (error) {
       return this.handleError(error);
@@ -187,6 +172,7 @@ export class ContentService extends BaseService implements IContentService {
   // 컨텐츠 삭제
   public async delete(contentId: number): Promise<IServiceResponse> {
     try {
+      // 컨텐츠 조회
       const prismaContent = await this.prisma.content.findUnique({
         where: {
           id: contentId,
@@ -201,16 +187,11 @@ export class ContentService extends BaseService implements IContentService {
 
       // 컨텐츠 삭제
       await this.prisma.content.update({
-        where: {
-          id: contentId,
-        },
-        data: {
-          isDeleted: true,
-          updatedAt: new Date(),
-        },
+        where: { id: contentId },
+        data: { isDeleted: true, updatedAt: new Date() },
       });
 
-      // 성공
+      // 응답 성공
       return { result: true };
     } catch (error) {
       return this.handleError(error);
@@ -220,39 +201,18 @@ export class ContentService extends BaseService implements IContentService {
   // 컨텐츠 목록 조회
   public async list(groupId: number, data: IRequestContents): Promise<IServiceResponse<IContent[] | []>> {
     try {
-      // 페이지 번호가 없거나 1보다 작은 경우 1로 설정
-      if (!data.page || data.page < 1) {
-        data.page = 1;
-      }
+      // 기본값 설정
+      const page = Math.max(1, data.page || 1);
+      const pageSize = Math.min(100, Math.max(1, data.pageSize || 10));
+      const sort = data.sort || 'ID_ASC';
 
-      // 페이지 크기가 작거나 너무 크면 10으로 설정
-      if (!data.pageSize || data.pageSize < 1 || data.pageSize > 100) {
-        data.pageSize = 10;
-      }
-
-      // 정렬이 없는 경우 id로 설정
-      if (!data.sort) {
-        data.sort = 'ID_ASC';
-      }
-
-      let orderBy = {};
-      if (data.sort === 'ID_DESC') {
-        orderBy = {
-          id: 'desc',
-        };
-      } else if (data.sort === 'ID_ASC') {
-        orderBy = {
-          id: 'asc',
-        };
-      } else if (data.sort === 'TITLE_DESC') {
-        orderBy = {
-          title: 'desc',
-        };
-      } else if (data.sort === 'TITLE_ASC') {
-        orderBy = {
-          title: 'asc',
-        };
-      }
+      // 정렬 조건 설정
+      const orderBy = {
+        ID_DESC: { id: 'desc' as const },
+        ID_ASC: { id: 'asc' as const },
+        TITLE_DESC: { title: 'desc' as const },
+        TITLE_ASC: { title: 'asc' as const },
+      }[sort] || { id: 'asc' as const };
 
       // 컨텐츠 그룹 정보 조회
       const groupInfo = await this.prisma.contentGroup.findUnique({
@@ -268,80 +228,72 @@ export class ContentService extends BaseService implements IContentService {
         throw new NotFoundError('컨텐츠 그룹이 존재하지 않습니다.');
       }
 
-      // 전체 컨텐츠 수 조회
-      const totalContents = await this.prisma.content.count({
-        where: {
-          groupId,
-          title: data.query ? { contains: data.query } : {},
-          isDeleted: false,
-          isActivated: true,
-        },
-      });
-
-      // 컨텐츠 목록 조회
-      const prismaContents = await this.prisma.content.findMany({
-        where: {
-          groupId,
-          title: data.query ? { contains: data.query } : {},
-          isDeleted: false,
-          isActivated: true,
-        },
-        skip: (data.page - 1) * data.pageSize,
-        take: data.pageSize,
-        orderBy: orderBy,
-      });
-
-      // 컨텐츠 목록 생성
-      const contents: IContent[] = prismaContents.map((content) => {
-        // 날짜 형식 변환
-        const createdAtToString = formatDateToString(content.createdAt.toISOString(), true, true, true);
-        const updatedAtToString = formatDateToString(content.updatedAt?.toISOString(), true, true, true);
-
-        // 컨텐츠 정보 생성
-        return {
-          id: content.id,
-          groupId: content.groupId,
-          title: content.title,
-          content: content.content ?? null,
-          writerId: content.writerId ?? null,
-          writerName: content.writerName ?? null,
-          writerEmail: content.writerEmail ?? null,
-          writerPhone: content.writerPhone ?? null,
-          viewCount: content.viewCount ?? 0,
-          likeCount: content.likeCount ?? 0,
-          commentCount: content.commentCount ?? 0,
-          isAnonymous: content.isAnonymous,
-          isNotice: content.isNotice ?? false,
-          isActivated: content.isActivated,
-          ip: content.ip ?? null,
-          userAgent: content.userAgent ?? null,
-          createdAt: createdAtToString as string,
-          updatedAt: (updatedAtToString as string) || null,
-        };
-      });
-
-      // 메타데이터 생성
-      const metadata = {
-        group: {
-          id: groupInfo.id,
-          kind: groupInfo.kind,
-          title: groupInfo.title,
-          description: groupInfo.description ?? null,
-          banners: {
-            top: groupInfo.bannerTopUrl ?? null,
-          },
-        },
-        total: totalContents,
-        page: data.page,
-        pageSize: data.pageSize,
-        start: (data.page - 1) * data.pageSize + 1,
-        end: (data.page - 1) * data.pageSize + contents.length,
-        count: contents.length,
-        totalPage: Math.ceil(totalContents / data.pageSize),
+      // 검색 조건 설정
+      const where = {
+        groupId,
+        isDeleted: false,
+        isActivated: true,
+        ...(data.query ? { title: { contains: data.query } } : {}),
       };
 
-      // 성공
-      return { result: true, metadata, data: contents };
+      // 전체 컨텐츠 수 조회
+      const [totalContents, prismaContents] = await Promise.all([
+        this.prisma.content.count({ where }),
+        this.prisma.content.findMany({
+          where,
+          skip: (page - 1) * pageSize,
+          take: pageSize,
+          orderBy,
+        }),
+      ]);
+
+      // 컨텐츠 목록 생성
+      const contents: IContent[] = prismaContents.map((content) => ({
+        id: content.id,
+        groupId: content.groupId,
+        title: content.title,
+        content: content.content ?? null,
+        writerId: content.writerId ?? null,
+        writerName: content.writerName ?? null,
+        writerEmail: content.writerEmail ?? null,
+        writerPhone: content.writerPhone ?? null,
+        viewCount: content.viewCount ?? 0,
+        likeCount: content.likeCount ?? 0,
+        commentCount: content.commentCount ?? 0,
+        isAnonymous: content.isAnonymous,
+        isNotice: content.isNotice ?? false,
+        isActivated: content.isActivated,
+        ip: content.ip ?? null,
+        userAgent: content.userAgent ?? null,
+        createdAt: formatDateToString(content.createdAt.toISOString(), true, true, true) as string,
+        updatedAt: content.updatedAt
+          ? (formatDateToString(content.updatedAt.toISOString(), true, true, true) as string)
+          : null,
+      }));
+
+      // 응답 성공
+      return {
+        result: true,
+        metadata: {
+          group: {
+            id: groupInfo.id,
+            kind: groupInfo.kind,
+            title: groupInfo.title,
+            description: groupInfo.description ?? null,
+            banners: {
+              top: groupInfo.bannerTopUrl ?? null,
+            },
+          },
+          total: totalContents,
+          page,
+          pageSize,
+          start: (page - 1) * pageSize + 1,
+          end: (page - 1) * pageSize + contents.length,
+          count: contents.length,
+          totalPage: Math.ceil(totalContents / pageSize),
+        },
+        data: contents,
+      };
     } catch (error) {
       return this.handleError(error);
     }
@@ -349,7 +301,7 @@ export class ContentService extends BaseService implements IContentService {
 
   public async groupInfo(groupId: number): Promise<IServiceResponse<IContentGroup>> {
     try {
-      // 컨텐츠 그룹 정보 조회
+      // 컨텐츠 그룹 조회
       const prismaResult = await this.prisma.contentGroup.findUnique({
         where: {
           id: groupId,
@@ -363,6 +315,7 @@ export class ContentService extends BaseService implements IContentService {
         throw new NotFoundError('컨텐츠 그룹이 존재하지 않습니다.');
       }
 
+      // 컨텐츠 그룹 정보 생성
       const groupInfo: IContentGroup = {
         id: prismaResult.id,
         kind: prismaResult.kind,
@@ -384,7 +337,7 @@ export class ContentService extends BaseService implements IContentService {
         isActivated: prismaResult.isActivated,
       };
 
-      // 성공
+      // 응답 성공
       return { result: true, metadata: { group: { id: groupId } }, data: groupInfo };
     } catch (error) {
       return this.handleError(error);
