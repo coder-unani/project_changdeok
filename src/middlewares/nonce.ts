7976d1fbbb4f495715287b1d4d0723c0b7657ecc
coder-unani <coder.unani@gmail.com>
2025-05-06 17:55:49 +0900

    [fix] GTM을 위한 CSP 정책 수정

--- src/middlewares/nonce.ts
+++ src/middlewares/nonce.ts
@@ -13,12 +13,14 @@ export class NonceMiddleware implements IMiddleware {
     // Add nonce to response headers for CSP
     const cspDirectives = [
       "default-src 'self'",
-      `script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://tagassistant.google.com`,
+      `script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://tagassistant.google.com https://www.google-analytics.com`,
       `style-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://fonts.googleapis.com`,
-      "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' https://www.googletagmanager.com data:",
-      "connect-src 'self' https://www.googletagmanager.com",
+      "font-src 'self' https://fonts.gstatic.com data:",
+      "img-src 'self' https://www.googletagmanager.com https://fonts.gstatic.com data:",
+      "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com",
       "frame-src 'self' https://www.googletagmanager.com",
+      "worker-src 'self' blob:",
+      "child-src 'self' blob:",
     ].join('; ');
 
     res.setHeader('Content-Security-Policy', cspDirectives);
