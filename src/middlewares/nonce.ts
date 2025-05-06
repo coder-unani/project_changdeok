1417a7090b1dcdb8cf3417b1ba9b0d51ff43a637
coder-unani <coder.unani@gmail.com>
2025-05-06 17:58:22 +0900

    [fix] GTM을 위한 CSP 정책 수정

--- src/middlewares/nonce.ts
+++ src/middlewares/nonce.ts
@@ -13,17 +13,25 @@ export class NonceMiddleware implements IMiddleware {
     // Add nonce to response headers for CSP
     const cspDirectives = [
       "default-src 'self'",
-      `script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://tagassistant.google.com https://www.google-analytics.com`,
-      `style-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://fonts.googleapis.com`,
+      `script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://tagassistant.google.com https://www.google-analytics.com https://www.google.com https://www.gstatic.com`,
+      `style-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://fonts.googleapis.com https://www.google.com https://www.gstatic.com`,
       "font-src 'self' https://fonts.gstatic.com data:",
-      "img-src 'self' https://www.googletagmanager.com https://fonts.gstatic.com data:",
-      "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com",
-      "frame-src 'self' https://www.googletagmanager.com",
+      "img-src 'self' https://www.googletagmanager.com https://fonts.gstatic.com https://www.google.com https://www.gstatic.com data:",
+      "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://www.gstatic.com",
+      "frame-src 'self' https://www.googletagmanager.com https://www.google.com",
       "worker-src 'self' blob:",
       "child-src 'self' blob:",
+      "base-uri 'self'",
+      "form-action 'self'",
+      "frame-ancestors 'self'",
+      "object-src 'none'",
+      'upgrade-insecure-requests',
     ].join('; ');
 
     res.setHeader('Content-Security-Policy', cspDirectives);
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
 
     next();
   }
