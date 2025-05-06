782e02c28b055e6792d8cd750cc50e5c150a3c7a
coder-unani <coder.unani@gmail.com>
2025-05-06 17:42:05 +0900

    [fix] 코드 수정

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "** 서비스 재시작 **\n"
+echo "** 서비스 재시작 **"
 
 # Stop the services
 ./stop.sh
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Start nginx service
-echo "** nginx 서비스 시작 **\n"
+echo "** nginx 서비스 시작 **"
 sudo systemctl start nginx.service
 
 # Start the application in the background
-echo "** 애플리케이션 백그라운드 시작 **\n"
+echo "** 애플리케이션 백그라운드 시작 **"
 node ./dist/app.js & 
