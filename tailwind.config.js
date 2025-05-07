79a46bdd233206ae00dbb90dd6c645aa6d42a98c
hwakk <97441379+hwakk@users.noreply.github.com>
2025-05-08 00:34:54 +0900

    [fix] 스타일 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,8 @@ module.exports = {
   content: ['./src/views/**/*.ejs', './src/styles/**/*.css'],
   theme: {
     extend: {
-      colors: colors,
       fontFamily: {
-        sans: ['var(--default-font-family)', ...defaultTheme.fontFamily.sans],
+        sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
       },
     },
   },
