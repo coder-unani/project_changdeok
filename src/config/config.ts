b4ebfa2ec9c3e39aa958e0416af208511f1c37e5
coder-unani <coder.unani@gmail.com>
2025-05-13 19:24:19 +0900

    [refactor] Config 설정 객체화

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,117 +1,187 @@
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+import dotenv from 'dotenv';
 
-// 프로젝트 설정
-export const CONFIG = {
-  /**
-   * 실행 환경
-   */
-  ENV: process.env.NODE_ENV || 'development',
-  /**
-   * 서비스 URL
-   */
-  SERVICE_URL: process.env.SERVICE_URL || '',
-  /**
-   * 서비스 포트
-   */
-  SERVICE_PORT: Number.isNaN(parseInt(process.env.SERVICE_PORT || ''))
-    ? 3000
-    : parseInt(process.env.SERVICE_PORT || '3000'),
-  /**
-   * 정적 파일 경로
-   */
-  STATIC_PATH: process.env.STATIC_PATH || undefined,
-  /**
-   * 로그 경로 및 레벨
-   */
-  LOG_PATH: process.env.LOG_PATH || undefined,
-  LOG_LEVEL: process.env.LOG_LEVEL || undefined,
+import { prisma } from '../library/database';
+import { ISettings } from '../types/config';
+import { ICORSOptions } from '../types/config';
 
-  MAX_FILE_UPLOAD_SIZE: Number(process.env.MAX_FILE_UPLOAD_SIZE) || 20, // 20MB
+dotenv.config();
+
+export class Config {
+  private static instance: Config;
+  private settings: ISettings | null = null;
+
+  private constructor() {}
+
+  // 싱글톤 인스턴스 반환
+  public static getInstance = () => {
+    if (!Config.instance) {
+      Config.instance = new Config();
+    }
+    return Config.instance;
+  };
+
+  public init = async () => {
+    if (this.settings) {
+      return;
+    }
+
+    // 사이트 셋팅 조회
+    const prismaData = await prisma.settings.findUnique({
+      where: {
+        id: 1,
+      },
+    });
+
+    // 사이트 셋팅이 존재하지 않으면 예외 발생
+    if (!prismaData) {
+      throw new Error('사이트 셋팅이 존재하지 않습니다.');
+    }
+
+    // 사이트 셋팅 데이터 반환
+    const settings: ISettings = {
+      title: prismaData?.title || '',
+      introduction: prismaData?.introduction || '',
+      description: prismaData?.description || '',
+      keywords: prismaData?.keywords || '',
+      favicon: prismaData?.favicon || '',
+      logo: prismaData?.logo || '',
+      ogTagJson: prismaData?.ogTagJson || '',
+      companyJson: prismaData?.companyJson || '',
+      serviceDomain: prismaData?.serviceDomain || '',
+      servicePort: prismaData?.servicePort || 80,
+      expressDomain: prismaData?.expressDomain || '',
+      expressPort: prismaData?.expressPort || 80,
+      maxUploadSize: prismaData?.maxUploadSize || 20,
+      jwtExpireSecond: prismaData?.jwtExpireSecond || 3600,
+      enabledTagsJson: prismaData?.enabledTagsJson || '',
+      enabledCorsJson: prismaData?.enabledCorsJson || '',
+      createdAt: prismaData?.createdAt || new Date(),
+      updatedAt: prismaData?.updatedAt || null,
+    };
+
+    // 사이트 셋팅 데이터 저장
+    this.settings = settings;
+  };
+
+  // 실행 환경 선언 변수
+  public getEnv = () => {
+    return process.env.NODE_ENV || 'development';
+  };
+
+  // 사이트 셋팅 데이터 반환
+  public getSettings = () => {
+    if (!this.settings) {
+      throw new Error('사이트 셋팅이 초기화되지 않았습니다.');
+    }
+    return this.settings;
+  };
+
+  // express 도메인 반환
+  public getExpressDomain = () => {
+    return this.settings?.expressDomain || 'http://localhost';
+  };
+
+  // express 포트 반환
+  public getExpressPort = (): number => {
+    return Number(this.settings?.expressPort) || 3000;
+  };
+
+  // express URL 반환
+  public getExpressUrl = () => {
+    const port = this.getExpressPort();
+    const domain = this.getExpressDomain();
+
+    return !port || port === 80 || port === 443 ? domain : `${domain}:${port}`;
+  };
+
+  // 정적 파일 경로 반환
+  public getStaticPath = () => {
+    return process.env.STATIC_PATH || '';
+  };
+
+  // 로그 경로 반환
+  public getLogPath = () => {
+    return process.env.LOG_PATH || '';
+  };
+
+  // 로그 레벨 반환
+  public getLogLevel = () => {
+    return process.env.LOG_LEVEL || '';
+  };
+
   /**
-   * Crypto 비밀키 (32바이트)
+   * JWT 비밀키 반환
+   * openssl rand -base64 32 (납품처가 변경될 때마다 새로운 키를 생성)
+   * @returns string
+   */
+  public getJwtSecretKey = (): string => {
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error('JWT_SECRET_KEY가 설정되지 않았습니다.');
+    }
+    return process.env.JWT_SECRET_KEY;
+  };
+
+  // Crypto 비밀키 반환
+  /**
+   * Crypto 비밀키 반환
    * openssl rand -base64 32 (납품처가 변경될 때마다 새로운 키를 생성)
    * *** 이 키를 분실 할 경우.. 데이터 복구가 불가능합니다. ***
+   * @returns string
    */
-  CRYPTO_SECRET_KEY: process.env.CRYPTO_SECRET_KEY ? process.env.CRYPTO_SECRET_KEY : '',
-  /**
-   * JWT 비밀키 (32바이트)
-   * openssl rand -base64 32 (납품처가 변경될 때마다 새로운 키를 생성)
-   */
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ? process.env.JWT_SECRET_KEY : '',
-  /**
-   * JWT 만료 시간
-   * 1시간 = 3600초
-   * 1일 = 86400초
-   * 1주일 = 604800초
-   */
-  JWT_EXPIRE_SECOND: Number.isNaN(parseInt(process.env.JWT_EXPIRE_SECOND || ''))
-    ? 3600
-    : parseInt(process.env.JWT_EXPIRE_SECOND || '3600'),
-  /**
-   * Google reCAPTCHA 설정
-   */
-  RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY || '',
-  RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY || '',
-};
+  public getCryptoSecretKey = (): string => {
+    if (!process.env.CRYPTO_SECRET_KEY) {
+      throw new Error('CRYPTO_SECRET_KEY가 설정되지 않았습니다.');
+    }
+    return process.env.CRYPTO_SECRET_KEY;
+  };
 
-export const CORS_API_OPTIONS = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  maxAge: 86400, // 24시간
-  optionsSuccessStatus: 200,
-};
+  // Google reCAPTCHA 사이트 키 반환
+  public getRecaptchaSiteKey = (): string => {
+    if (!process.env.RECAPTCHA_SITE_KEY) {
+      throw new Error('RECAPTCHA_SITE_KEY가 설정되지 않았습니다.');
+    }
+    return process.env.RECAPTCHA_SITE_KEY;
+  };
 
-export const CORS_OPTIONS = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-};
+  // Google reCAPTCHA 비밀 키 반환
+  public getRecaptchaSecretKey = (): string => {
+    if (!process.env.RECAPTCHA_SECRET_KEY) {
+      throw new Error('RECAPTCHA_SECRET_KEY가 설정되지 않았습니다.');
+    }
+    return process.env.RECAPTCHA_SECRET_KEY;
+  };
 
-export const CORS_BACKEND_OPTIONS = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-};
+  public getCORSApiOptions = (): ICORSOptions => {
+    return {
+      origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+      maxAge: 86400, // 24시간
+      optionsSuccessStatus: 200,
+    };
+  };
 
-// 허용할 HTML 태그
-export const ALLOWED_TAGS = [
-  'h1',
-  'h2',
-  'h3',
-  'h4',
-  'h5',
-  'h6',
-  'blockquote',
-  'p',
-  'a',
-  'ul',
-  'ol',
-  'nl',
-  'li',
-  'b',
-  'i',
-  'strong',
-  'em',
-  'strike',
-  'code',
-  'hr',
-  'br',
-  'div',
-  'table',
-  'thead',
-  'caption',
-  'tbody',
-  'tr',
-  'th',
-  'td',
-  'pre',
-  'iframe',
-  'img',
-  'span',
-  'font',
-  'del',
-  'ins',
-];
+  public getCORSBackendOptions = (): ICORSOptions => {
+    return {
+      origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+      optionsSuccessStatus: 200,
+    };
+  };
+
+  public getCORSFrontendOptions = (): ICORSOptions => {
+    return {
+      origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+      optionsSuccessStatus: 200,
+    };
+  };
+}
+
+export const asyncConfig = async () => {
+  const config = Config.getInstance();
+  await config.init();
+  return config;
+};
 
 export const PERMISSIONS_DEFINE = {
   1: '최고 관리자',
@@ -121,32 +191,3 @@ export const PERMISSIONS_DEFINE = {
   5: '광고 관리',
   6: '통계 관리',
 };
-/**
- * 허용하는 날짜 형식
- * 2023-10-15
- * 2023/10/15
- * 2023-10-15 14:30
- * 2023/10/15 14:30
- * 2023-10-15T14:30
- * 2023-10-15 14:30:00
- * 2023/10/15 14:30:00
- * 2023-10-15T14:30:00
- * 1981-01-12T00:00:00.000Z
- */
-export const REG_DATE_PATTERN =
-  /^(\d{4}[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12]\d|3[01]))(?:[T\s](?:((?:[01]\d|2[0-3]):[0-5]\d)(?::(?:[0-5]\d)(?:\.\d{3})?)?)(Z)?)?$/;
-
-/**
- * 이메일 형식 체크
- */
-export const REG_EMAIL_PATTERN = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-/**
- * 비밀번호 8자리 이상, 숫자, 문자, 특수문자 포함 체크
- */
-export const REG_PASSWORD_PATTERN = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
-
-/**
- * 전화번호 형식 체크
- */
-export const REG_PHONE_PATTERN = /^\d{9,11}$/;
